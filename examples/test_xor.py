c4c0062173072f8f2efb88dba4bae454cf19228d
jkrue242 <josephkrueger242@gmail.com>
2025-12-01 23:58:09 -0600

    comment

--- examples/test_xor.py
+++ examples/test_xor.py
@@ -1,2 +1,7 @@
+# in order to import this, you need to first build the entire project 
+# this will create a cpDLL.so shared library file that python can load
+# and thus lets you import it here.
+
+# todo: see if i can use this to run CUDA on google colab?
 import cpDLL 
 cpDLL.test_xor()
